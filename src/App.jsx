75eeb001c08854af96f3675b8778a0bc5bb209cb
DamianAf <damian.afanasii01@gmail.com>
2025-08-16 21:09:31 +0100

    Finished the Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import Hero from './components/Hero.jsx';
 
 const App = () => {
   return (
-    <main>
-      <h1 className='text-5xl text-violet-300 font-bold'>Welcome to Awwwards</h1>
+    <main className='relative min-h-screen min-w-screen overflow-x-hidden'>
+      <Hero />
+      <section className='z-0 min-h-screen bg-blue-500' />
     </main>
   )
 }
