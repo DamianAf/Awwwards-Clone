697c8386b049bf66bfc9541dfde76d23ff878f4a
DamianAf <damian.afanasii01@gmail.com>
2025-08-17 21:07:23 +0100

    Created the About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import About from './components/About.jsx';
 import Hero from './components/Hero.jsx';
 
 const App = () => {
   return (
     <main className='relative min-h-screen min-w-screen overflow-x-hidden'>
       <Hero />
-      <section className='z-0 min-h-screen bg-blue-500' />
+      <About />
     </main>
   )
 }
